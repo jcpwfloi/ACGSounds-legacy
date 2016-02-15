d467500bdf9c1d7f37b2ee9299d0ad312d5f89dc
jcpwfloi <jcpwfloi@gmail.com>
2016-02-15 14:46:22 +0800

    search page

--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+    res.render('list', {
+        title: req.__('Search')
+    })
+});
+
 router.get('/:text', function(req, res) {
-    res.send(req.params.text);
+    res.render('list', {
+        title: req.__('Search') + ' - ' + req.params.text,
+        text: req.params.text
+    })
 });
 
 module.exports = router;
