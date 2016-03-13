1f523aa501e38d332238e3903fa98a54bbaba60d
jcpwfloi <jcpwfloi@gmail.com>
2016-03-13 19:36:05 +0800

    prototype added

--- classes/src/pagination.js
+++ classes/src/pagination.js
@@ -1,12 +1,17 @@
 (function(window, document, undefined) {'use strict';
- function Pagination(opt) {
+ function Pagination(opt, data) {
      this.defaults = {
-         offset: 5
+         offset: 5,
+         current: 1
      };
 
      this.options = {};
+
      extend(this.options, this.defaults, opt);
  }
+
+ Pagination.prototype = {
+ };
 
  window.Pagination = Pagination;
 })(window, document);
