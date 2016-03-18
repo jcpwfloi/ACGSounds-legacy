4d3b0117f2d6d2270b41c0e646e00589d95cfe45
Pisces000221 <1786762946@qq.com>
2016-03-18 22:53:17 +0800

    Fix 'network error' is shown incorrectly

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -5,14 +5,18 @@ function login() {
         _csrf: $('meta[name=csrf-token]').attr('content')
     };
 
-    $.post('/api/login', data, function(res) {
+    $.post('/api/login', data, function (res) {
         if (res.msg == 'Success') {
             if ($.urlParam('callback'))
                 window.location.href = $.urlParam('callback');
             else window.location.href = '/';
         } else Materialize.toast('未知错误', 3000, 'rounded');
-    }, "json").error(function(e) {
-        Materialize.toast('网络错误', 3000, 'rounded');
+    }, "json").error(function (e) {
+        if (e.responseJSON.msg === 'ENOTMATCH') {
+            Materialize.toast('用户名或密码不正确', 3000, 'rounded');
+        } else {
+            Materialize.toast('未知错误 &gt; &lt;<br>Message: ' + e.responseJSON.msg, 3000, 'rounded');
+        }
     });
 }
 
